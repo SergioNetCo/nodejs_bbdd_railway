e08005f7d563aa4ea70a602df4c27d28485b7c9e
SergioNetCo <83806626+SergioNetCo@users.noreply.github.com>
2024-01-29 18:06:24 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 export const PORT = process.env.PORT || 3000
 export const DB_HOST = process.env.DB_HOST || 'roundhouse.proxy.rlwy.net'
 export const DB_USER = process.env.DB_USER || 'root'
-export const DB_PASSWORD = process.env.DB_PASSWORD || 'a32aGfAB6G2cfDeAH4h5d1G1dHeCDg6g'
+export const DB_PASSWORD = process.env.DB_PASSWORD || '5b545C3e-6G-B413ghC4chE6e46fHF4H'
 export const DB_NAME = process.env.DB_NAME || 'railway'
-export const DB_PORT = process.env.DB_PORT || 55393
+export const DB_PORT = process.env.DB_PORT || 10071
